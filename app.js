03d77d01cc24095da13a073392d294f687e7da2b
MAL501 <marcosaragonlopez@gmail.com>
2025-01-21 16:15:51 +0000

    SEGUNDO COMMIT

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-require('dotenv').config();
-const express=require("express");
+require('dotenv').config()
+const express = require('express');
 const app = express();
-const port= 8099;
+const port = process.env.PORT || 80;
 
-app.use(express.static("public"));
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}));
 
-app.get("/",(req,res)=>{
-    res.send("Bienvenido a mi aplicación web")
+app.get('/', (req,res)=>{
+    res.send('Bienvenido a mi aplicacion web')
 })
 
-app.get("/saludo",(req,res)=>{
-    const nombre=req.query.nombre ||"invitado";
-    const passwd=req.query.passwd ||"invitado apellidos";
-    res.send("Hola "+nombre+"<br>Filtramos tu contraseña: "+passwd);
-    res.sendFile(__dirname+"/public/index.html");
-});
-
-app.listen(port,()=>{
-    console.log(`Servidor escuchando por el puerto ${port}`)
+app.post('/saludo', (req,res)=>{
+    const nombre = req.body.nombre || 'invitado';
+    const contrasena = req.body.contrasena || '1234';
+    res.send('Saludo ' + nombre + ' ' + contrasena)
 })
 
-
-
-
-app.get("*",(req,res)=>{
-    res.sendFile(__dirname+"/public/404.html");
+app.get('/precios', (req,res)=>{
+    res.send('<h1>>Precios</h1><hr><p>Ordenador:1000</p>')
 })
+app.get('*', (req,res)=>{
+    res.sendFile(__dirmane + '/public/404.html')
+})
+
+app.post('/factorial', (req,res)=>{
+    const numero = req.body.numero;
+    if(numero===undefined || isNaN(numero)){
+        return res.status(400).send('Numero no valido');    
+    }
+    const factorial = (n)=>{
+        if(n===0) return 1;
+        return n * factorial(n-1);
+    }
+    const resultado = factorial(Number(numero));
+    res.send('el facotirla de  ' + numero + 'es ' + resultado)
+})
+
+app.listen(port,
+    ()=>console.log(`Servidor escuchando por el puerto ${port}`));
