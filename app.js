25e83ac8bbd8448c456b4ccc5f815631dfcd113c
MAL501 <marcosaragonlopez@gmail.com>
2025-01-21 16:52:38 +0000

    Tercer commit

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-require('dotenv').config()
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 80;
+const express = require('express')
+const app = express()
+app.set("view-engine","ejs");
+app.get('/', function (req, res) {
+    const datos=[
+        {nombre:"Ana",apellidos:"Morales"},
+        {nombre:"Juan",apellidos:"Juan"}
 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}));
-
-app.get('/', (req,res)=>{
-    res.send('Bienvenido a mi aplicacion web')
+    ];
+  res.render("index",{datos:datos});
 })
 
-app.post('/saludo', (req,res)=>{
-    const nombre = req.body.nombre || 'invitado';
-    const contrasena = req.body.contrasena || '1234';
-    res.send('Saludo ' + nombre + ' ' + contrasena)
-})
-
-app.get('/precios', (req,res)=>{
-    res.send('<h1>>Precios</h1><hr><p>Ordenador:1000</p>')
-})
-app.get('*', (req,res)=>{
-    res.sendFile(__dirmane + '/public/404.html')
-})
-
-app.post('/factorial', (req,res)=>{
-    const numero = req.body.numero;
-    if(numero===undefined || isNaN(numero)){
-        return res.status(400).send('Numero no valido');    
-    }
-    const factorial = (n)=>{
-        if(n===0) return 1;
-        return n * factorial(n-1);
-    }
-    const resultado = factorial(Number(numero));
-    res.send('el facotirla de  ' + numero + 'es ' + resultado)
-})
-
-app.listen(port,
-    ()=>console.log(`Servidor escuchando por el puerto ${port}`));
+app.listen(3000)
